8c43b19dc234edf4042673980d761207fe65fb13
Dmitry Tarantin <dimichspb@gmail.com>
2016-01-19 15:53:43 +0300

    Installing framework, initial configuration

---
+++ common/config/main.php
@@ -0,0 +1,18 @@
+<?php
+return [
+    'vendorPath' => dirname(dirname(__DIR__)) . '/vendor',
+    'components' => [
+        'cache' => [
+            'class' => 'yii\caching\FileCache',
+        ],
+        'urlManager' => [
+            'enablePrettyUrl' => true,
+            'showScriptName' => false,
+            'rules' => [
+                '<_c:[\w\-]+>/<id:\d+>' => '<_c>/view',
+                '<_c:[\w\-]+>' => '<_c>/index',
+                '<_c:[\w\-]+>/<_a:[\w\-]+>/<id:\d+>' => '<_c>/<_a>',
+            ],
+        ],
+    ],
+];
